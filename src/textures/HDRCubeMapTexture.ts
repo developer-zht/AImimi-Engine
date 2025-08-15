ee11811e18c7694a16eaafba95a9a664e9c00380
HT_Zhang <developer_zht@163.com>
2025-08-16 01:18:31 +0800

    feat: 添加了两个功能 - 承接了将DataTexture转换成WebGLTexture的工作 - 在generate mipmap后添加了LINEAR_MIPMAP_LINEAR

--- src/textures/HDRCubeMapTexture.ts
+++ src/textures/HDRCubeMapTexture.ts
@@ -3,6 +3,7 @@ import { TRSTransform } from '@/objects/Mesh'
 import { Shader } from '@/shaders/Shader'
 import { ShaderParameters } from '@/types/Shader'
 import { mat4 } from 'gl-matrix'
+import { DataTexture } from 'three'
 
 export class HDRCubeMapTexture {
   // 单例模式
@@ -37,7 +38,7 @@ export class HDRCubeMapTexture {
     }
   }
 
-  async init(hdrTexture: WebGLTexture, resolution: number = 512) {
+  async init(dataTexture: DataTexture, resolution: number = 512) {
     console.time('🟠 IBL Precomputation')
 
     // 设置 IBL Framebuffer
@@ -50,7 +51,7 @@ export class HDRCubeMapTexture {
 
     // 计算环境 cubemap
     console.log('🟠 Converting HDR to Cubemap...')
-    this.envCubemap = this.convertHDRToCubemap(hdrTexture, resolution)
+    this.envCubemap = this.convertHDRToCubemap(dataTexture, resolution)
 
     // 计算辐照度图
     // console.log('🟠 Computing irradiance map...')
@@ -138,7 +139,135 @@ export class HDRCubeMapTexture {
   /**
    * 将 Equirectangular HDR 贴图转换为 Cubemap
    */
-  private convertHDRToCubemap(hdrTexture: WebGLTexture, resolution: number): WebGLTexture {
+  // private convertHDRToCubemap(hdrTexture: WebGLTexture, resolution: number): WebGLTexture {
+  //   // 创建 Cubemap 纹理
+  //   const cubemap = this.gl.createTexture()
+  //   if (!cubemap) {
+  //     throw new Error('❌ Failed to create WebGL texture')
+  //   }
+  //   this.gl.bindTexture(this.gl.TEXTURE_CUBE_MAP, cubemap)
+
+  //   // 为 6 个面分配内存
+  //   for (let i = 0; i < 6; i++) {
+  //     this.gl.texImage2D(
+  //       this.gl.TEXTURE_CUBE_MAP_POSITIVE_X + i,
+  //       0,
+  //       this.gl.RGB,
+  //       resolution,
+  //       resolution,
+  //       0,
+  //       this.gl.RGB,
+  //       this.gl.FLOAT,
+  //       null
+  //     )
+  //   }
+
+  //   // 设置纹理参数
+  //   this.gl.texParameteri(this.gl.TEXTURE_CUBE_MAP, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE)
+  //   this.gl.texParameteri(this.gl.TEXTURE_CUBE_MAP, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE)
+  //   this.gl.texParameteri(
+  //     this.gl.TEXTURE_CUBE_MAP,
+  //     this.gl.TEXTURE_MIN_FILTER,
+  //     this.gl.LINEAR_MIPMAP_LINEAR
+  //   )
+  //   this.gl.texParameteri(this.gl.TEXTURE_CUBE_MAP, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR)
+
+  //   /**
+  //    * MVP Matrices
+  //    * 特殊的"捕获相机" （非场景相机）Project Matrix
+  //    * - 90度FOV：确保每个面捕获正好1/6的环境
+  //    * - 宽高比1.0：因为cubemap每个面是正方形
+  //    * - 近远平面：包围渲染的立方体
+  //    */
+  //   const captureProjectionMatrix = mat4.perspective(mat4.create(), Math.PI / 2, 1, 0.1, 10)
+  //   const captureViewMatrices = this.getCaptureViewMatrices()
+  //   const captureModelMatrix = this.getCaptureModelMatrix()
+
+  //   // 开始渲染 FBO
+  //   this.gl.viewport(0, 0, resolution, resolution)
+  //   this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.conversionFBO)
+
+  //   for (let i = 0; i < 6; i++) {
+  //     // 将 cubemap 的 TEXTURE_CUBE_MAP_POSITIVE_X + i 面 bind 到当前的 FRAMEBUFFER 的 COLOR_ATTACHMENT0
+  //     this.gl.framebufferTexture2D(
+  //       this.gl.FRAMEBUFFER,
+  //       this.gl.COLOR_ATTACHMENT0,
+  //       this.gl.TEXTURE_CUBE_MAP_POSITIVE_X + i,
+  //       cubemap,
+  //       0
+  //     )
+
+  //     /**
+  //      * gl.clear() 放在 gl.framebufferTexture2D() ​​之后​​是出于​​绑定关系​​和​​渲染流程​​的合理设计
+  //      * 在绑定完成前​​，帧缓冲区的目标附件是未确定的，此时调用 gl.clear() 可能无效，因为不清楚要清除的是哪个缓冲区
+  //      * 在绑定完成后​​，gl.clear() 明确作用于当前绑定的帧缓冲区及其关联的立方体贴图面
+  //      */
+  //     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT)
+
+  //     // 使用 equirectangular 转 cubemap 的 shader
+  //     this.equirectToCubemapShader.use()
+  //     this.equirectToCubemapShader.setMat4('uModelMatrix', captureModelMatrix)
+  //     this.equirectToCubemapShader.setMat4('uViewMatrix', captureViewMatrices[i])
+  //     this.equirectToCubemapShader.setMat4('uProjectionMatrix', captureProjectionMatrix)
+  //     this.equirectToCubemapShader.setTexture2D('uEquirectangularMap', hdrTexture, 0)
+
+  //     // 渲染立方体
+  //     this.renderCube()
+  //   }
+
+  //   // 生成 mipmap
+  //   this.gl.bindTexture(this.gl.TEXTURE_CUBE_MAP, cubemap)
+  //   this.gl.generateMipmap(this.gl.TEXTURE_CUBE_MAP)
+
+  //   this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null)
+
+  //   return cubemap
+  // }
+
+  private convertHDRToCubemap(dataTexture: DataTexture, resolution: number): WebGLTexture {
+    // DataTexture -> WebGLTexture
+    const { data, width, height } = dataTexture.source.data
+
+    // 判断被加载文件的通道类型
+    const externalFormat = data.length / (width * height) === 3 ? this.gl.RGB : this.gl.RGBA
+    // 内部数据格式与外部数据格式保持一致
+    const internalFormat = externalFormat
+    // 外部数据类型
+    let format = 0
+    // 目前 data 的类型一直为 Float32Array
+    if (data instanceof Uint8Array) {
+      format = this.gl.UNSIGNED_BYTE
+    } else if (data instanceof Float32Array) {
+      format = this.gl.FLOAT
+    }
+    console.log(`🔘 DataTexture: ${dataTexture}`)
+    const hdrTexture: WebGLTexture = this.gl.createTexture()
+    if (!hdrTexture) {
+      throw new Error('❌ 无法创建 WebGL 纹理对象')
+    }
+    this.gl.bindTexture(this.gl.TEXTURE_2D, hdrTexture)
+    // this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, true)
+    // this.gl.pixelStorei(this.gl.UNPACK_ALIGNMENT, 1)
+    this.gl.texImage2D(
+      this.gl.TEXTURE_2D,
+      0,
+      internalFormat,
+      width,
+      height,
+      0,
+      externalFormat,
+      format,
+      data
+    )
+    // 设置 Texture 参数
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE)
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE)
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR)
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR)
+
+    this.gl.bindTexture(this.gl.TEXTURE_2D, null)
+    console.log(`🔘 EXRTexture: ${hdrTexture}`)
+
     // 创建 Cubemap 纹理
     const cubemap = this.gl.createTexture()
     if (!cubemap) {
@@ -151,24 +280,20 @@ export class HDRCubeMapTexture {
       this.gl.texImage2D(
         this.gl.TEXTURE_CUBE_MAP_POSITIVE_X + i,
         0,
-        this.gl.RGB,
+        internalFormat,
         resolution,
         resolution,
         0,
-        this.gl.RGB,
-        this.gl.FLOAT,
+        externalFormat,
+        format,
         null
       )
     }
 
-    // 设置纹理参数
+    // 设置 Texture 参数
     this.gl.texParameteri(this.gl.TEXTURE_CUBE_MAP, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE)
     this.gl.texParameteri(this.gl.TEXTURE_CUBE_MAP, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE)
-    this.gl.texParameteri(
-      this.gl.TEXTURE_CUBE_MAP,
-      this.gl.TEXTURE_MIN_FILTER,
-      this.gl.LINEAR_MIPMAP_LINEAR
-    )
+    this.gl.texParameteri(this.gl.TEXTURE_CUBE_MAP, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR)
     this.gl.texParameteri(this.gl.TEXTURE_CUBE_MAP, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR)
 
     /**
@@ -217,6 +342,11 @@ export class HDRCubeMapTexture {
     // 生成 mipmap
     this.gl.bindTexture(this.gl.TEXTURE_CUBE_MAP, cubemap)
     this.gl.generateMipmap(this.gl.TEXTURE_CUBE_MAP)
+    this.gl.texParameteri(
+      this.gl.TEXTURE_CUBE_MAP,
+      this.gl.TEXTURE_MIN_FILTER,
+      this.gl.LINEAR_MIPMAP_LINEAR
+    )
 
     this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null)
 
@@ -233,7 +363,7 @@ export class HDRCubeMapTexture {
 
     const positionLocation = this.equirectToCubemapShader.getAtrributeLocation('aVertexPosition')
     if (positionLocation === -1) {
-      console.log(`❌ Attribute '${name}' not found in shader`)
+      console.log(`❌ Attribute aVertexPosition not found in shader`)
       return
     }
     this.gl.enableVertexAttribArray(positionLocation)
