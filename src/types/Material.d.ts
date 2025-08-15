98789a2d45f5fc6a0a010c080553563f7af616d5
HT_Zhang <developer_zht@163.com>
2025-08-15 12:06:18 +0800

    feat: 添加了enum UniformType,增强了Uniforms中的type的robust

--- src/types/Material.d.ts
+++ src/types/Material.d.ts
@@ -1,3 +1,18 @@
+export enum UniformType {
+  ONE_I = '1i',
+  TWO_IV = '2iv',
+  THREE_IV = '3iv',
+
+  ONE_F = '1f',
+  TWO_FV = '2fv',
+  THREE_FV = '3fv',
+
+  MATRIX_4FV = 'matrix4fv',
+
+  TEXTURE_2D = 'texture',
+  TEXTURE_CUBE = 'textureCube'
+}
+
 export interface Uniforms {
-  [name: string]: { type: string; value: any }
+  [name: string]: { type: UniformType; value: any }
 }
