8f7ae93e9cc20443d3a19f5612bad4f65e7c7202
HT_Zhang <developer_zht@163.com>
2025-09-03 23:54:55 +0800

    refactor: 删除了原本应该在方法外 进行的操作，使得setTexUniform()职责单一化

--- src/materials/SkyboxMaterial.ts
+++ src/materials/SkyboxMaterial.ts
@@ -1,13 +1,10 @@
 import { getShaderString } from '@/loaders/loadShader'
-import { CubeMapTexture } from '@/textures/CubeMapTexture'
 import { Material } from '@/materials/Material'
 import { Uniforms, UniformType } from '@/types/Material'
-import { HDRCubeMapTexture } from '@/textures/HDRCubeMapTexture'
-import { TextureType } from '@/managers/cubemap/CubeMapRenderManager'
 
 export class SkyboxMaterial extends Material {
   private gl: WebGLRenderingContext
-  private cubeMapTexture: CubeMapTexture
+  // private cubeMapTexture: CubeMapTexture
   constructor(
     gl: WebGLRenderingContext,
     vertexShaderContent: string,
@@ -21,25 +18,10 @@ export class SkyboxMaterial extends Material {
     }
     super(baseUniforms, [], vertexShaderContent, fragmentShaderContent, null)
     this.gl = gl
-    this.cubeMapTexture = new CubeMapTexture(this.gl)
   }
 
-  async setTexUniform(textureType: TextureType = TextureType.HDR_FILE) {
-    try {
-      switch (textureType) {
-        case TextureType.CUBE_MAP:
-          await this.cubeMapTexture.createCubeMapFromImages({
-            basePath: '/assets/textures/skyboxes/sky_09_cubemap/',
-            extension: '.png'
-          })
-          this.uniforms['uSkyboxMap'].value = this.cubeMapTexture.texture
-          break
-        case TextureType.HDR_FILE:
-          this.uniforms['uSkyboxMap'].value = HDRCubeMapTexture.getInstance(this.gl).envCubemap
-      }
-    } catch (error) {
-      throw new Error(error)
-    }
+  setTexUniform(texture: WebGLTexture) {
+    this.uniforms['uSkyboxMap'].value = texture
   }
 }
 
@@ -47,13 +29,13 @@ export async function buildSkyboxMaterial(
   gl: WebGLRenderingContext,
   vertexPath: string,
   fragmentPath: string,
-  textureType: TextureType
+  texture: WebGLTexture
 ): Promise<SkyboxMaterial> {
   const vertexShaderContent = await getShaderString(vertexPath)
   const fragmentShaderContent = await getShaderString(fragmentPath)
 
   const skyboxMaterial = new SkyboxMaterial(gl, vertexShaderContent, fragmentShaderContent)
-  await skyboxMaterial.setTexUniform(textureType)
+  skyboxMaterial.setTexUniform(texture)
   // console.log(skyboxMaterial.uniforms['uSkyboxMap'].value)
 
   return skyboxMaterial
