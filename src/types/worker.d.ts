3d30858246f614c440c8e90d393cce9fe9a5b20a
HT_Zhang <developer_zht@163.com>
2025-09-10 01:03:23 +0800

    refactor(delete): 删除了多余的类型声明

--- src/types/worker.d.ts
+++ src/types/worker.d.ts
@@ -1,26 +1,3 @@
-type SpatialKeys =
-  | 'serializedSpectrum'
-  | 'serializedHeightSpectrum'
-  | 'serializedSlopeXSpectrum'
-  | 'serializedSlopeZSpectrum'
-  | 'serializedDispXSpectrum'
-  | 'serializedDispZSpectrum'
-
-// Web Worker 无法直接传类实例
-// 因此该 interface 定义无效
-// interface FFTWorkerBasicMessage {
-//   fftProcessor: FFTProcessor
-// }
-
-type serializedSpectrum = {
-  real: number
-  imag: number
-}[][]
-
-export type FFTWorkerMessage = {
-  [key in SpatialKeys]?: serializedSpectrum
-}
-
 export interface SerializedSpectrum {
   realArray: Float32Array
   imagArray: Float32Array
