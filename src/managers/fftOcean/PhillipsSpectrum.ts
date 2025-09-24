4d3a6a9d05694df8123742615b3f825bb316ea2b
HT_Zhang <developer_zht@163.com>
2025-09-24 23:04:12 +0800

    refactor: 重构了Phillips Spectrum的计算方式

--- src/managers/fftOcean/PhillipsSpectrum.ts
+++ src/managers/fftOcean/PhillipsSpectrum.ts
@@ -1,18 +1,4 @@
-interface WindDirection {
-  x: number
-  y: number
-}
-
-export interface OceanParams {
-  size: number // 海面片元尺寸（米）
-  resolution: number // 网格分辨率（必须是2的幂）
-  windSpeed: number // 风速 (m/s)
-  windDirection: WindDirection // 风向（归一化）
-  gravity: number // 重力加速度
-  choppiness: number // 波浪尖锐度
-  depth?: number // 水深（可选，用于有限深度）
-  amplitude?: number
-}
+import { OceanParams } from '@/types/fftOcean'
 
 /** Phillips 波谱公式：
  * P(k) = A * exp(-(1 / (kL)^2)) / k^4 * ( (k_hat · w_hat)^2 )
@@ -28,7 +14,13 @@ export interface OceanParams {
  * - 如果 (k_hat · w_hat < 0) 则表示逆风波，此时可将 P(k) 设为 0   // 抑制逆风波
  */
 export class PhillipsSpectrum {
-  // Phillips 常数，影响整体幅度，如果太小，高频会被过度压制
+  /* Phillips 常数，影响整体幅度，如果太小，高频会被过度压制
+      很弱的波浪 A = 0.002 
+      轻微波浪   A = 0.005  
+      标准波浪   A = 0.008 ⭐推荐起始值
+      较强波浪   A = 0.015
+      很强的波浪 A = 0.025 
+  */
   private A = 0.008
   constructor() {}
 
@@ -93,12 +85,23 @@ export class PhillipsSpectrum {
       return 0
     } else if (k > 1) {
       // 对高频增强: 高频波数 > 1 时增强
-      return phillips * 2
+      // return phillips * 2
+      const k_cutoff = 2.0
+      return phillips * Math.exp(-(k / k_cutoff) * (k / k_cutoff))
     } else {
       return phillips
     }
 
     // return kDotWind < 0 ? 0 : k > 1.0 ? phillips * 2 : phillips
     // return phillips
+  }
+
+  calculateH0Magnitude(kx: number, kz: number, params: OceanParams): number {
+    const P = this.calculate(kx, kz, params)
+
+    // h0_magnitude = sqrt(P/2)
+    const h0Magnitude = Math.sqrt(P / 2)
+
+    return h0Magnitude
   }
 }
