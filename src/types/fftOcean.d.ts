093314008efc7c733f6381eda26cd0e043d34690
HT_Zhang <developer_zht@163.com>
2025-10-09 23:04:11 +0800

    refactor: 注释掉了不需要的interface并按照主从关系重构了文件结构

--- src/types/fftOcean.d.ts
+++ src/types/fftOcean.d.ts
@@ -9,7 +9,7 @@ export interface FFTOceanRenderManagerConfig {
   materialParams: FFTOceanMaterialParams
 
   // FFT Ocean
-  // oceanParams?: OceanParams
+  // oceanParams?: OceanParams // Deprecated
   cascadeConfig: CascadeConfig
 }
 
@@ -18,18 +18,29 @@ interface WindDirection {
   y: number
 }
 
-export interface OceanParams {
-  size: number // 海面片元尺寸（米）
-  resolution: number // 网格分辨率（必须是2的幂）
-  windSpeed: number // 风速 (m/s)
-  windDirection: WindDirection // 风向（归一化）
-  gravity: number // 重力加速度
-  choppiness: number // 波浪尖锐度
-  depth?: number // 水深（用于有限深度）
-  fetch?: number // 风区大小
-  swellMixing?: number // ξ 参数，默认0（纯风浪）
-  amplitude?: number // 能量缩放系数
-  // cascadeConfig?: CascadeConfig // 可选的 cascade 配置
+// Deprecated
+// export interface OceanParams {
+//   size: number // 海面片元尺寸（米）
+//   resolution: number // 网格分辨率（必须是2的幂）
+//   windSpeed: number // 风速 (m/s)
+//   windDirection: WindDirection // 风向（归一化）
+//   gravity: number // 重力加速度
+//   choppiness: number // 波浪尖锐度
+//   depth?: number // 水深（用于有限深度）
+//   fetch?: number // 风区大小
+//   swellMixing?: number // ξ 参数，默认0（纯风浪）
+//   amplitude?: number // 能量缩放系数
+//   // cascadeConfig?: CascadeConfig // 可选的 cascade 配置
+// }
+
+// Cascade 配置
+export interface CascadeConfig {
+  renderingMode: RenderingMode
+  enabled: boolean // 是否启用 cascade，true 为 cascade，false 为 single
+  meshResolution: number // 目标统一分辨率，默认使用最高层分辨率
+  meshSize: number // 目标统一范围，默认使用最大范围
+  blendMode: BlendMode // 混合模式：相加或加权
+  layerParamsSet: CascadeLayerParams[] // cascade 层级配置
 }
 
 // Cascade Layer 配置
@@ -46,11 +57,12 @@ export interface CascadeLayerParams {
   amplitude?: number // 能量缩放系数
 }
 
-// Cascade 配置
-export interface CascadeConfig {
-  enabled: boolean // 是否启用 cascade，true 为 cascade，false 为 single
-  meshResolution: number // 目标统一分辨率，默认使用最高层分辨率
-  meshSize: number // 目标统一范围，默认使用最大范围
-  blendMode: 'additive' | 'weighted' // 混合模式：相加或加权
-  layerParamsSet: CascadeLayerParams[] // cascade 层级配置
+export enum BlendMode {
+  ADDITIVE = 'additive',
+  WEIGHT = 'weighted'
+}
+
+export enum RenderingMode {
+  LINE = 'line',
+  MESH = 'mesh'
 }
