616fe7d9188a209fa30b0acc7800a31d49e13b78
developer_zht <developerzht@gmail.com>
2026-01-18 02:04:49 +0800

    refactor(utils): 对 utils/ 路径下的文件重命名

--- src/utils/helpers.ts
+++
@@ -1,5 +0,0 @@
-import { ExhaustiveMatchError } from '@/errors/LanguageError/ExhaustiveMatchError'
-
-export function assertNever(value: never, message?: string) {
-  throw new ExhaustiveMatchError(value, message)
-}
