dbe019291f421931da1aa60ff2d67bdfa62daf16
developer_zht <developerzht@gmail.com>
2026-04-14 23:53:29 +0800

    feat(src/lights/visualizers/ & src/lights/types/): 添加灯光可视化工具并更新灯光类型定义

--- src/lights/types/light.d.ts
+++ src/lights/types/light.d.ts
@@ -1,6 +1,19 @@
-import type { mat3, mat4, vec3 } from 'gl-matrix'
-import type { Vec3 } from '../math/types/math'
+import type { mat3, mat4 } from 'gl-matrix'
 import type { UpdatedParamters } from '@/types/MeshRender'
+import { Vec3 } from '@/math/types/math'
+
+// ============================================================
+//  基础光源（纯粹的光照数据，和阴影无关）
+// ============================================================
+type LightType = 'directional' | 'point' | 'spot'
+
+export interface ILight {
+  type: LightType
+  position: Vec3
+  radiance: Vec3 // 最终辐射度（所有光源都有）
+}
+
+// =============== Deprecated ===============
 
 type LightUp = Vec3
 
@@ -11,9 +24,9 @@ interface LightDir {
 }
 
 export interface LightParams {
-  lightRadiance: Vec3
-  lightPos: Vec3
-  lightDir: LightDir
+  radiance: Vec3
+  position: Vec3
+  direction: LightDir
 }
 
 export interface UpdatedLightParamters {
@@ -21,16 +34,16 @@ export interface UpdatedLightParamters {
   uLightDir: Vec3
 }
 
-export interface Light {
-  mesh: Mesh
-  material: EmissiveMaterial
-  lightPos: LightParams['lightPos']
-  lightDir?: LightParams['lightDir']
-  lightUp?: LightUp
-  fbo: WebGLFramebuffer
+// export interface Light {
+//   mesh: Mesh
+//   material: EmissiveMaterial
+//   lightPos: LightParams['lightPos']
+//   lightDir?: LightParams['lightDir']
+//   lightUp?: LightUp
+//   fbo: WebGLFramebuffer
 
-  // Directional Light
-  CalcDirectionalShadingDirection?(): Vec3
-  CalcDirectionalLightVP?(): mat4
-  CalcDirectionalLightMVP?(translate: Vec3, scale: Vec3): mat4
-}
+//   // Directional Light
+//   CalcDirectionalShadingDirection?(): Vec3
+//   CalcDirectionalLightVP?(): mat4
+//   CalcDirectionalLightMVP?(translate: Vec3, scale: Vec3): mat4
+// }
---
+++ src/lights/visualizers/createLightVisualizer.ts
@@ -0,0 +1,48 @@
+import { Mesh } from '@/objects/Mesh'
+import { Transform } from '@/objects/utils/Transform'
+import { Shader } from '@/shaders/Shader-refactor'
+import { MeshRenderer } from '@/renderers/MeshRenderer'
+import { ILight } from '../types/light'
+import { ShaderPaths } from '@/shaders/_config/shaderPaths'
+import { DirectionalLight } from '../directionalLight/DirectionalLight-refactor'
+import { EmissiveMaterial } from '@/materials/lights/EmissiveMaterial'
+
+// 创建光源本身，也就是画光源小方块，并不创建光源产生的效果，光源产生的效果需要在每个场景中单独配置
+export async function createLightVisualizer(
+  gl: WebGLRenderingContext,
+  light: ILight
+): Promise<MeshRenderer> {
+  if (light.type === 'directional') {
+    // Mesh: worldSize × worldSize × worldSize 小立方体
+    const halfSize = (light as DirectionalLight).worldSize / 2
+    const transform = new Transform(light.position, [0, 0, 0], [halfSize, halfSize, halfSize]) // 不要忘记 cube 的大小是 2 x 2 x 2，因此缩放成 worldSize 只需 worldSize / 2
+    const mesh = Mesh.cube(transform, gl)
+
+    // Material: 只有 uLightRadiance
+    const material = new EmissiveMaterial(light.radiance)
+
+    // Shader: 内嵌的 LightCube shader
+    const shader = await Shader.createShader(
+      gl,
+      ShaderPaths.LIGHT_GIZMO_VERTEX,
+      ShaderPaths.LIGHT_GIZMO_FRAGMENT
+    )
+
+    return new MeshRenderer(gl, mesh, material, shader, 'DirectionalLightVisualizer')
+  }
+
+  if (light.type === 'point') {
+    // PointLight 可能想用球体、或者更小的方块、甚至不同的 shader
+    const transform = new Transform(light.position, [0, 0, 0], [0.05, 0.05, 0.05])
+    const mesh = Mesh.cube(transform, gl) // 以后可以换成 Mesh.sphere()
+    const material = new EmissiveMaterial(light.radiance)
+    const shader = await Shader.createShader(
+      gl,
+      ShaderPaths.LIGHT_GIZMO_VERTEX, // 当前碰巧和 directional 相同
+      ShaderPaths.LIGHT_GIZMO_FRAGMENT
+    )
+    return new MeshRenderer(gl, mesh, material, shader, 'PointLightVisualizer')
+  }
+
+  throw new Error(`Unknown light type: ${light.type}`)
+}
