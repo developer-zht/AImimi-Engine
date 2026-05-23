de71a46e8a3d0e21ff0f3e1a92458455c2050413
developer_zht <developerzht@gmail.com>
2026-05-24 02:49:01 +0800

    refactor(src/lights/directionalLight/_presets/): 更新 hw4 方向光预设

--- src/lights/directionalLight/_presets/hw4Lights.ts
+++ src/lights/directionalLight/_presets/hw4Lights.ts
@@ -4,7 +4,8 @@ import { DirectionalLight } from '../DirectionalLight-refactor'
 export const hw4Light = new DirectionalLight({
   radiance: [10, 10, 10],
   position: [60, 20, 30],
-  direction: [0, 0, -1],
+  target: [60, 20, 20],
+  // direction: [0, 0, -1],
   up: [0, 1, 0],
   worldSize: 1,
   shadowOptions: { shadowConfig: { orthoSize: 100, near: 0.1, far: 300 } }
