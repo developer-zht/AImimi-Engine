f31897b35b3d26d8ec50cdb7a9d953dad00f074f
developer_zht <developerzht@gmail.com>
2026-05-25 01:13:47 +0800

    refactor(src/math/types/): 更新数学模块类型定义

--- src/math/types/math.d.ts
+++ src/math/types/math.d.ts
@@ -1,5 +1,8 @@
-// 定义3D向量类型
+/** 自定义 2D 向量类型 */
+export type Vec2 = [number, number]
+
+/** 自定义 3D 向量类型 */
 export type Vec3 = [number, number, number]
 
-// 定义2D向量类型
-export type Vec2 = [number, number]
+/** 自定义 4D 向量类型 */
+export type Vec4 = [number, number, number, number]
